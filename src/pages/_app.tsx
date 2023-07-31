13e45fce0f3e854cbbad1f0478336e15853a6670
Gurkan <hello@gurkz.me>
2023-07-31 18:45:09 +0200

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
 import '@/styles/globals.css'
+import { Partytown } from '@builder.io/partytown/react'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>prettiest pokémon</title>
+        <Partytown forward={['datalayer.push']} />
       </Head>
+
+      <Script defer data-domain="pokemon.gurkz.me" src="https://analytics.gurkz.me/js/script.js" type="text/partytown"></Script>
       <main className='p-2'>
         <Component {...pageProps} />
       </main>
