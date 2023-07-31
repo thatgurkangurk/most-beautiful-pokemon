16f703c16b135300d9b217afa96d1815d6156a55
Gurkan <hello@gurkz.me>
2023-07-31 17:12:49 +0200

    add favicon and title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className='p-2'>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <title>prettiest pokémon</title>
+      </Head>
+      <main className='p-2'>
+        <Component {...pageProps} />
+      </main>
+    </>
+
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import Head from "next/head"
+
 export default function Home() {
   return (
-    <main>
-      <h1>work in progress...</h1>
-    </main>
+    <>
+      <Head>
+        <title>prettiest pokémon - home</title>
+      </Head>
+      <main>
+        <h1>work in progress...</h1>
+      </main>
+    </>
   )
 }
