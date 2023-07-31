60f9638dcfa4f69e6a81eebcdc628951d30320d6
Gurkan <hello@gurkz.me>
2023-07-31 19:26:16 +0200

    it only needs the domain

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -7,7 +7,7 @@ interface ProvidersProps {
 
 const Providers: FC<ProvidersProps> = ({ children }) => {
     return (
-        <PlausibleProvider selfHosted domain="pokemon.gurkz.me" customDomain="https://analytics.gurkz.me/js/script.tagged-events.js">
+        <PlausibleProvider selfHosted domain="pokemon.gurkz.me" customDomain="https://analytics.gurkz.me/">
             {children}
         </PlausibleProvider>
     )
