c806d71b91ec09bcecd694d66c36bce8b86cf751
Gurkan <hello@gurkz.me>
2023-08-01 10:44:37 +0200

    Revert "add loading spinner to pokémon button (should probably refactor this, a bit ugly)"
    it was broken in prod
    
    This reverts commit 7a569da5aa9cd9b352024d2f9b757baf4cb7ea69.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,15 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import { Dispatch, FC, SetStateAction, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { trpc } from '../utils/trpc'
 import { PokemonType } from '@/types/Pokemon'
 import Wavy from '@/components/WavyText'
 import { motion } from 'framer-motion'
 import { Fade } from '@/anims'
 import { usePlausible } from 'next-plausible'
-import { Loader2 } from 'lucide-react'
 
 const LEADERBOARD_NAV = '/prettiest'
-// Capitalize first letter of string
-const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1)
 
 const Home: NextPage = () => {
   const [pokemonList, setPokemonList] = useState<PokemonType[]>([])
@@ -36,51 +33,6 @@ const Home: NextPage = () => {
     setPokemonList([])
   }
 
-  if (!pokemon) return <p>Loading...</p>
-
-  return (
-    <>
-      <Head>
-        <title>prettiest pokémon</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-
-      {/* Make an either or choice between the 2 pokemon in pokeList */}
-      <div className='flex min-h-screen flex-col items-center justify-center py-2'>
-        <main className='flex w-full flex-col items-center justify-center px-20 text-center'>
-          <Wavy className='pb-2 sm:text-4xl md:text-6xl font-bold' text='which pokémon is prettiest?' heading='h1'></Wavy>
-          <Wavy heading='p' className='italic mb-6 text-gray-500' text={`a "study" ran by gurkan to see which pokémon the internet prefers`}></Wavy>
-          <div className='flex min-h-[15rem] items-center'>
-            <motion.div variants={Fade} className='relative sm:grid grid-cols-2 gap-4'>
-              <PokemonButton fetchedEnoughPokemon={fetchedEnoughPokemon} className='bg-red-400' pokemon={pokemonList[0]} resetPokemon={resetPokemon} />
-              <PokemonButton fetchedEnoughPokemon={fetchedEnoughPokemon} className='bg-green-400' pokemon={pokemonList[1]} resetPokemon={resetPokemon} />
-
-
-
-              {/* 'OR' seperator */}
-              <div className='pointer-events-none absolute inset-0 grid place-items-center'>
-                <div className='grid h-12 w-12 place-items-center rounded-full bg-black text-white outline outline-[8px] outline-white'>
-                  or
-                </div>
-              </div>
-            </motion.div>
-          </div>
-          <button onClick={() => resetPokemon()} className='mt-4 p-2 bg-orange-500 rounded-md shadow'>both are ugly</button>
-        </main>
-
-        <Link href={LEADERBOARD_NAV} className='pt-12 text-xs underline'>
-          See leaderboard
-        </Link>
-        <Link href="/privacy" className='pt-12 text-xs underline'>
-          Privacy Policy
-        </Link>
-      </div>
-    </>
-  )
-}
-
-const PokemonButton: FC<{ pokemon: PokemonType | undefined, resetPokemon: () => void, className: string, fetchedEnoughPokemon: boolean }> = ({ pokemon, resetPokemon, className: extClassName, fetchedEnoughPokemon }) => {
-  const plausible = usePlausible();
   // Handle pokemon voting
   const { mutate: vote } = trpc.pokemon.vote.useMutation({
     onSuccess: () => {
@@ -98,35 +50,86 @@ const PokemonButton: FC<{ pokemon: PokemonType | undefined, resetPokemon: () => 
     })
     vote({ id, imgUrl, name })
   }
-  return (
-    <button
-      type='button'
-      onClick={() =>
-        handleVote(
-          pokemon!.id,
-          pokemon!.sprites.front_default,
-          pokemon!.name
-        )
-      }
-      className={`rounded-m p-8 shadow w-full grid place-items-center ${extClassName}`}>
-      <PokemonImage pokemon={pokemon} />
-    </button>
-  )
-}
 
-const PokemonImage: FC<{ pokemon: PokemonType | undefined }> = ({ pokemon }) => {
-  if (!pokemon) return <Loader2 className='mr-2 h-16 w-16 animate-spin' />
+  if (!pokemon) return <p>Loading...</p>
+
+  // Capitalize first letter of string
+  const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1)
+
   return (
     <>
-      <h2 className='text-4xl font-bold'>{capitalize(pokemon.name)}</h2>
-      <div className='relative h-32 w-32'>
+      <Head>
+        <title>prettiest pokémon</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
 
-        <Image
-          src={pokemon.sprites.front_default}
-          alt={pokemon.name}
-          layout='fill'
-          objectFit='cover'
-        />
+      {/* Make an either or choice between the 2 pokemon in pokeList */}
+      <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+        <main className='flex w-full flex-col items-center justify-center px-20 text-center'>
+          <Wavy className='pb-2 sm:text-4xl md:text-6xl font-bold' text='which pokémon is prettiest?' heading='h1'></Wavy>
+          <Wavy heading='p' className='italic mb-6 text-gray-500' text={`a "study" ran by gurkan to see which pokémon the internet prefers`}></Wavy>
+          <div className='flex min-h-[15rem] items-center'>
+            {pokemonList[0] && pokemonList[1] && (
+              <motion.div variants={Fade} className='relative sm:grid grid-cols-2 gap-4'>
+                <button
+                  type='button'
+                  onClick={() =>
+                    handleVote(
+                      pokemonList[0]!.id,
+                      pokemonList[0]!.sprites.front_default,
+                      pokemonList[0]!.name
+                    )
+                  }
+                  className='rounded-md bg-green-400 p-8 shadow w-full grid place-items-center'>
+                  <h2 className='text-4xl font-bold'>{capitalize(pokemonList[0].name)}</h2>
+                  <div className='relative h-32 w-32'>
+                    <Image
+                      src={pokemonList[0].sprites.front_default}
+                      alt={pokemonList[0].name}
+                      layout='fill'
+                      objectFit='cover'
+                    />
+                  </div>
+                </button>
+
+                <button
+                  type='button'
+                  onClick={() =>
+                    handleVote(
+                      pokemonList[1]!.id,
+                      pokemonList[1]!.sprites.front_default,
+                      pokemonList[1]!.name
+                    )
+                  }
+                  className='rounded-md bg-red-400 p-8 shadow w-full grid place-items-center'>
+                  <h2 className='text-4xl font-bold'>{capitalize(pokemonList[1].name)}</h2>
+                  <div className='relative h-32 w-32'>
+                    <Image
+                      src={pokemonList[1].sprites.front_default}
+                      alt={pokemonList[1].name}
+                      layout='fill'
+                      objectFit='cover'
+                    />
+                  </div>
+                </button>
+
+                {/* 'OR' seperator */}
+                <div className='pointer-events-none absolute inset-0 grid place-items-center'>
+                  <div className='grid h-12 w-12 place-items-center rounded-full bg-black text-white outline outline-[8px] outline-white'>
+                    or
+                  </div>
+                </div>
+              </motion.div>
+            )}
+          </div>
+        </main>
+
+        <Link href={LEADERBOARD_NAV} className='pt-12 text-xs underline'>
+          See leaderboard
+        </Link>
+        <Link href="/privacy" className='pt-12 text-xs underline'>
+          Privacy Policy
+        </Link>
       </div>
     </>
   )
